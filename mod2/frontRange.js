67f3f30c5482adae61dc9bea6dd5348a5dd9dbf6
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-07-25 21:06:12 -0600

    Add fourteeners and begin to work through first two problems.

---
+++ mod2/frontRange.js
@@ -0,0 +1,192 @@
+frontRange: {
+  numberOfPeaks : 6,
+  highestPeak : 'Gray\'s Peak',
+  peaks : {
+    graysPeak : {
+      elevation : 14270,
+      rank : 9,
+      forest : 'Arapaho',
+      grizzlyBears : false,
+      marmots : true,
+      jerryLevel : 'critical',
+      numberOfRoutes : 4,
+      routes : {
+        northSlopes : {
+          mileage : 8,
+          gain : 3000,
+          difficulty : 'class 1',
+          exposure : 1
+        },
+        southRidge : {
+          mileage : 7,
+          gain : 3250,
+          difficulty : 'class 2',
+          exposure : 2
+        },
+        lostRatCouloir : {
+          mileage : 6.5,
+          gain : 3000,
+          difficulty : 'class 3',
+          exposure : 3
+        },
+        southWestRidge : {
+          mileage : 10.25,
+          gain : 3800,
+          difficulty : 'class 2',
+          exposure : 1
+        }
+      }
+    },
+    torreysPeak : {
+      elevation : 14267,
+      rank : 11,
+      forest : 'Arapaho',
+      grizzlyBears : false,
+      marmots : true,
+      jerryLevel : 'critical',
+      numberOfRoutes : 6,
+      routes : {
+        southSlopes : {
+          mileage : 8,
+          gain : 3000,
+          difficulty : 'class 2',
+          exposure : 1
+        },
+        kelsoRidge : {
+          mileage : 6.75,
+          gain : 3100,
+          difficulty : 'class 3',
+          exposure : 4
+        },
+        deadDogCouloir : {
+          mileage : 6.5,
+          gain : 3000,
+          difficulty : 'class 3',
+          exposure : 3
+        },
+        tuningFork : {
+          mileage : 10.5,
+          gain : 4500,
+          difficulty : 'class 2+',
+          exposure : 2
+        },
+        emperorCouloir : {
+          mileage : 9.5,
+          gain : 4500,
+          difficulty : 'class 3',
+          exposure : 3
+        },
+        westRidge : {
+          mileage : 10,
+          gain : 5500,
+          difficulty : 'class 2',
+          exposure : 2
+        }
+      }
+    },
+    longsPeak : {
+      elevation : 14255,
+      rank : 15,
+      forest : 'Rocky Mountain National Park',
+      grizzlyBears : false,
+      marmots : true,
+      jerryLevel : 'extreme',
+      numberOfRoutes : 3,
+      routes : {
+        keyhole : {
+          mileage : 14.5,
+          gain : 5100,
+          difficulty : 'class 3',
+          exposure : 4
+        },
+        loft : {
+          mileage : 13,
+          gain : 5300,
+          difficulty : 'class 3',
+          exposure : 4
+        },
+        keplingersCouloir : {
+          mileage : 16,
+          gain : 5900,
+          difficulty : 'class 3',
+          exposure : 4
+        }
+      }
+    },
+    mountEvans : {
+      elevation : 14262,
+      rank : 14,
+      forest : 'Arapaho',
+      grizzlyBears : false,
+      marmots : true,
+      jerryLevel: 'critical',
+      numberOfRoutes : 4,
+      routes : {
+        westRidge : {
+          mileage : 17,
+          gain : 5600,
+          difficulty : 'class 2',
+          exposure : 2
+        },
+        northeastFace : {
+          mileage : 3.5,
+          gain : 1475,
+          difficulty : 'class 2',
+          exposure : 1
+        },
+        northFace : {
+          mileage : 2,
+          gain : 1500,
+          difficulty : 'class 3',
+          exposure : 4
+        }
+      }
+    },
+    mountBierstadt : {
+      elevation : 14060,
+      rank : 38,
+      forest : 'Arapaho',
+      grizzlyBears: false,
+      marmots : true,
+      jerryLevel : 'critical',
+      numberOfRoutes : 2,
+      routes : {
+        westSlopes : {
+          mileage : 7,
+          gain : 2850,
+          difficulty : 'class 2',
+          exposure : 1
+        },
+        eastRidge : {
+          mileage : 5,
+          gain : 3000,
+          difficulty : 'class 3',
+          exposure : 4
+        }
+      }
+    },
+    pikesPeak : {
+      elevation : 14100,
+      rank : 30,
+      forest : 'Pike',
+      grizzlyBears : false,
+      marmots : true,
+      jerryLevel : 'extreme',
+      numberOfRoutes : 2,
+      routes : {
+        eastSlopes : {
+          mileage : 26,
+          gain : 7500,
+          difficulty : 'class 1',
+          exposure : 0
+        },
+        northwestSlopes : {
+          mileage : 14,
+          gain : 4300,
+          difficulty : 'class 2',
+          exposure : 1
+        }
+      }
+    }
+  }
+},
